57d7a926bac3b9db2461de388d576519dcf8c774
okpalaChidiebere <43583810+okpalaChidiebere@users.noreply.github.com>
2018-11-24 05:26:46 -0600

    Create config.h

---
+++ BinaryTree/config.h
@@ -0,0 +1,8 @@
+/**
+ * BSTree class (Lab ) configuration file.
+ * Activate test 'N' by defining the corresponding LAB_TESTN to have the value 1.
+ * Deactive test 'N' by setting the value to 0.
+ */
+
+#define LAB9_TEST1	1		// Programming Exercise : getCount
+#define LAB9_TEST2	1		// Programming Exercise : getHeight
